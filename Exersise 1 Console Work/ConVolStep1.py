e61f30fbd6bb95f39819779660524ca31081d2cb
Cole Herman <cole.herman@GCCYJPQMJ1WK.local>
2019-09-17 11:31:34 -0400

    exersise 1/step1

---
+++ Exersise 1 Console Work/ConVolStep1.py
@@ -0,0 +1,11 @@
+#Input
+#What inputs are needed to calculate the volume of a cylinder?
+
+#Process
+#What formula is used to calculate the volume of a cylinder?
+
+#Output
+#What is important about the output?
+
+x = 3 + 4 x 2
+print(x)
